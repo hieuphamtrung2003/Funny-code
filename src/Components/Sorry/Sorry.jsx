d5fda4b4dec6d701383718f870a699b0d31a448a
SE172998_PhamTrungHieu <hieuptse172998@fpt.edu.vn>
2024-07-24 17:54:45 +0700

    add

---
+++ src/Components/Sorry/Sorry.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Sorry.scss";
+
+function Sorry() {
+    const navigate = useNavigate();
+    const noButtonRef = useRef(null);
+
+    const handleSuccessClick = () => {
+        navigate("/success");
+    };
+
+    const moveButton = () => {
+        const button = noButtonRef.current;
+        const container = button.parentElement;
+
+        const containerRect = container.getBoundingClientRect();
+        const buttonRect = button.getBoundingClientRect();
+
+        const maxLeft = containerRect.width - buttonRect.width;
+        const maxTop = containerRect.height - buttonRect.height;
+
+        const randomLeft = Math.random() * maxLeft;
+        const randomTop = Math.random() * maxTop;
+
+        button.style.left = `${randomLeft}px`;
+        button.style.top = `${randomTop}px`;
+    };
+
+    return (
+        <div className="sorry-container">
+            <div className="gif-image">
+                <img src="https://media1.giphy.com/media/Lfi2G7BzY15333JtK1/giphy.gif?cid=6c09b952t4c32macs3n3jmiyirf7akhexsol4drb6ifspstl&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g" alt="Sorry GIF" />
+            </div>
+            <div className="text">
+                chị oi, em xin lũi mừ. Chị tha lỗi cho em nhaaaa
+            </div>
+            <div className="buttons">
+                <button onClick={handleSuccessClick}>okeee nè</button>
+                <button ref={noButtonRef} onMouseEnter={moveButton} className="move-button">không</button>
+            </div>
+        </div>
+    );
+}
+
+export default Sorry;
---
+++ src/Components/Sucess/Sucess.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sucess.scss";
+
+function Success() {
+    return (
+        <div className="sorry-container">
+            <div className="gif-image">
+                <img src="https://media0.giphy.com/media/Yle9Yz9izeVRyiwavn/giphy.gif?cid=6c09b952ljyk0iw8qmx25qajrj7zek5y18hiymwzwz03jb7q&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="Success GIF" />
+            </div>
+            <div className="text">
+                eo ơi, yêu thế. Hứa ko seen nữa mà
+            </div>
+        </div>
+    );
+}
+
+export default Success;
